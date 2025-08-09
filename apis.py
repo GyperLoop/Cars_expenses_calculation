f6a7d1ae3d139f479bcf4175f6045005768eb834
GyperLoop <garfieldfinucky@gmail.com>
2025-08-09 12:00:17 +0300

    Some_files

---
+++ apis.py
@@ -0,0 +1,14 @@
+def get_gas_price():
+    # TODO: Get price from API
+    return 8
+
+
+def get_power_price():
+    # TODO: Get price from API
+    return 1.2
+
+
+#print("I am in apis.py outside main")
+
+if __name__ == '__main__':
+    print("I am in apis.py")
